f25659ad86eeda35f5bd7fb379420bb091b5d6b5
Mursalin Leon <mursalinleon92@gmail.com>
2026-05-04 21:32:20 +0600

    loginDto & registration password compare

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { RegisterDto } from "./dto/register.dto";
+import { LoginDto } from "./dto/login.dto";
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -8,13 +9,33 @@ export class AuthService {
     constructor(private readonly prisma: PrismaService) { }
 
     async register(dto: RegisterDto) {
+
+        if (dto.password != dto.confirmPassword) {
+            throw new UnauthorizedException('Passwords do not match');
+        }
+
         const hashedPassword = await bcrypt.hash(dto.password, 10);
-        console.log(hashedPassword);
-        return this.prisma.user.create({
+        const user = await this.prisma.user.create({
             data: {
                 email: dto.email,
                 password: hashedPassword,
             }
         });
+
+        return user;
+    }
+
+    async login(dto: LoginDto) {
+        const user = await this.prisma.user.findUnique({
+            where: { email: dto.email },
+        });
+        if (!user) {
+            throw new UnauthorizedException('Incorrect email or password');
+        }
+        const isPasswordValid = await bcrypt.compare(dto.password, user.password);
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Incorrect email or password');
+        }
+        return user;
     }
 }
