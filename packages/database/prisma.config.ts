029a3d0c0502b458f8f9e52057ed56b7b28d0a25
Mursalin Leon <mursalinleon92@gmail.com>
2026-05-10 01:10:38 +0600

    edited on database and prisma config

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,5 +1,12 @@
-export default {
+import * as path from 'node:path';
+import * as dotenv from 'dotenv';
+import { defineConfig } from 'prisma/config';
+
+dotenv.config({ path: path.join(__dirname, '.env') });
+
+export default defineConfig({
+  schema: path.join(__dirname, 'prisma', 'schema.prisma'),
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
-};
+});
